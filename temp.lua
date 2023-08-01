c3c99a09e45c723e2f3029fbd37dff035cef2a7b
TRTX <62772966+younes19992@users.noreply.github.com>
2023-08-02 00:34:05 +0100

    Update temp.lua

--- temp.lua
+++ temp.lua
@@ -1,100 +1,73 @@
--- Define the module table
-local MapList = {}
-
--- Initialize the Rank table with the existing maps
-MapList.Rank = {
-    easylvl = {
-        {name = "Construct", coords = {}},
-        {name = "Indoor Courtyard", coords = {}}
-    },
-    normallvl = {
-        {name = "Arid Ruins", coords = {}},
-        {name = "Backrooms", coords = {}},
-        {name = "Seraph Research", coords = {}},
-        {name = "Underground Facility", coords = {}},
-        {name = "Four Corners", coords = {}},
-        {name = "Ikea", coords = {}},
-        {name = "Silver Mall", coords = {}},
-        {name = "Laboratory", coords = {}},
-        {name = "Crossroads", coords = {}},
-        {name = "Neighborhood", coords = {}},
-        {name = "Icebreaker", coords = {}},
-        {name = "Tudor Manor", coords = {}},
-        {name = "Drab", coords = {}},
-        {name = "Elysium Tower", coords = {}},
-        {name = "Kyoto", coords = {}},
-        {name = "Alpine Palace", coords = {}},
-        {name = "Winter City", coords = {}},
-        {name = "Nuclear Processing", coords = {}},
-        {name = "Elysium Moonbase", coords = {}},
-
-        -- Add other maps here
-    },
-    hardlvl = {
-        {name = "Desert Bus", coords = {}},
-        {name = "Maze", coords = {}},
-        {name = "Poolrooms", coords = {}},
-        {name = "Facade", coords = {}},
-        {name = "Library", coords = {}},
-        {name = "Mansion", coords = {}},
-        {name = "Jungle", coords = {}},
-        {name = "Station", coords = {}},
-        {name = "Catacombs", coords = {}},
-        {name = "Warped Estate", coords = {}},
-        {name = "Insane Asylum", coords = {}},
-        {name = "Work Facility", coords = {}},
-        {name = "Mayday", coords = {}},
-        {name = "Cliffshire", coords = {}},
-        {name = "Alleyways", coords = {}},
-        {name = "Terror Hotel", coords = {}},
-        -- Add other maps here
-    },
-    expertlvl = {
-        {name = "Traprooms", coords = {}},
-        {name = "Train Terminal", coords = {}},
-        {name = "Scorching Outpost", coords = {}},
-        -- Add other maps here
-    },
-    rarelvl = {
-        {name = "Trimp", coords = {}},
-        -- Add other maps here
-    }
+local locations = {
+    {name = "Construct", coords = {{10, 29, 23}, {53, 12, 63}, {40, 15, 37}}},
+    {name = "Indoor Courtyard", coords = {}},
+    {name = "Arid Ruins", coords = {}},
+    {name = "Backrooms", coords = {}},
+    {name = "Seraph Research", coords = {}},
+    {name = "Underground Facility", coords = {}},
+    {name = "Four Corners", coords = {}},
+    {name = "Ikea", coords = {}},
+    {name = "Silver Mall", coords = {}},
+    {name = "Laboratory", coords = {}},
+    {name = "Crossroads", coords = {}},
+    {name = "Neighborhood", coords = {}},
+    {name = "Icebreaker", coords = {}},
+    {name = "Tudor Manor", coords = {}},
+    {name = "Drab", coords = {}},
+    {name = "Elysium Tower", coords = {}},
+    {name = "Kyoto", coords = {}},
+    {name = "Alpine Palace", coords = {}},
+    {name = "Winter City", coords = {}},
+    {name = "Nuclear Processing", coords = {}},
+    {name = "Elysium Moonbase", coords = {}},
+    {name = "Desert Bus", coords = {}},
+    {name = "Maze", coords = {}},
+    {name = "Poolrooms", coords = {}},
+    {name = "Facade", coords = {}},
+    {name = "Library", coords = {}},
+    {name = "Mansion", coords = {}},
+    {name = "Jungle", coords = {}},
+    {name = "Station", coords = {}},
+    {name = "Catacombs", coords = {}},
+    {name = "Warped Estate", coords = {}},
+    {name = "Insane Asylum", coords = {}},
+    {name = "Work Facility", coords = {}},
+    {name = "Mayday", coords = {}},
+    {name = "Cliffshire", coords = {}},
+    {name = "Alleyways", coords = {}},
+    {name = "Terror Hotel", coords = {}},
+    {name = "Traprooms", coords = {}},
+    {name = "Train Terminal", coords = {}},
+    {name = "Scorching Outpost", coords = {}},
+    {name = "Trimp", coords = {}},
+    
 }
 
--- Function to get the entire map list
-function MapList.GetMapList()
-    return MapList.Rank
+-- Function to get count and all coordinates for a given location
+local function getAllCoordinates(location)
+    local count = #location.coords
+    return count, location.coords
 end
 
--- Function to get the list of maps for a specific difficulty level
-function MapList.GetMapsByDifficulty(difficulty)
-    return MapList.Rank[difficulty]
-end
-
--- Function to get the list of coordinates for a specific map
-function MapList.GetCoordinatesForMap(mapName)
-    for _, level in pairs(MapList.Rank) do
-        for _, map in ipairs(level) do
-            if map.name == mapName then
-                return map.coords
-            end
+-- Function to get coordinates for a given map name
+local function getCordinates(mapname)
+    for _, location in ipairs(locations) do
+        if location.name == mapname then
+            local locationCount, locationCoordinates = getAllCoordinates(location)
+            return { numofcoords = locationCount, coords = locationCoordinates }
         end
     end
-    return nil -- Map not found
+    return { numofcoords = 0, coords = {} }
 end
 
--- Function to add coordinates to a map in the format {name = "Construct", coords = {10, 20, 30}}
--- function MapList.AddCoordinatesToMap(mapName, coords)
---     for _, level in pairs(MapList.Rank) do
---         for _, map in ipairs(level) do
---             if map.name == mapName then
---                 map.coords = coords
---                 return true
---             end
---         end
---     end
---     return false -- Map not found
--- end
+-- Function to format coordinates as a string
+local function formatCoordinates(coordsSet)
+    return "{" .. coordsSet[1] .. "," .. coordsSet[2] .. "," .. coordsSet[3] .. "}"
+end
 
--- Return the module table
-return MapList
+-- Function to pick a specific coordinate by index
+local function pickCoordinateByIndex(coordinates, index)
+    if index >= 1 and index <= #coordinates then
+        return coordinates[index]
+    end
+end
